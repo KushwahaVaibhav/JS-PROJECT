15c979f6374691c7525d4bfce4716ad0c9a3c995
KushwahaVaibhav <vaibhab2015@gmail.com>
2021-06-26 17:23:11 +0530

    add delete feature

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const taskcontainer=document.querySelector(".task_container");
 
+let globalStore=[];
+
 const generateNewCard=(taskData) =>` 
-<div class="col-md-6 col-lg-4" id=${taskData.id}>
+<div class="col-md-6 col-lg-4" >
 <div class="card text-center d flex justify-content-end gap-2">
     <div class="card-header">
         <button type="button" class="btn btn-outline-success">
             <i class="fas fa-pencil-alt"></i>
             
         </button>
-        <button type="button" class="btn btn-outline-danger">
-            <i class="fas fa-trash-alt"></i></button>
+        <button type="button" class="btn btn-outline-danger"id=${taskData.id} onclick="deleteCard.apply(this,arguements)">
+            <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguements)"></i></button>
     </div>
     <img src=${taskData.imageUrl}>
     <div class="card-body">
@@ -27,6 +29,21 @@ const generateNewCard=(taskData) =>`
 </div>
 `;
 
+const loadIniitialCardData = () =>{
+
+    const getCardData= localStorage.getItem("tasKy");
+
+    const {cards}=JSON.parse(getCardData);
+
+    cards.map((cardObject)=>{
+        taskContainer.insertAdjacentHTML("beforeend",generateNewCard(cardObject));
+    
+        globalStore.push(cardObject);
+
+    })
+
+};
+
 
 const saveChanges=()=>{
     const taskData={
@@ -38,5 +55,47 @@ const saveChanges=()=>{
     };
   
 
-    taskContainer.insertAdjacentHTML("beforeend",generateNewCard(taskdata))
+    taskContainer.insertAdjacentHTML("beforeend",generateNewCard(taskData));
+    
+    globalStore.push(taskData);
+
+    localStorage.setItem("tasKy", JSON.stringify({cards:globalStore}));
+
+
 };
+
+
+const deleteCard=(event)=>{
+    event=window.event;
+
+    const targetID = event.target.id;
+    const tagname=event.target.tagName;
+    globalStore=globalStore.filter((cardObject)=> cardObject.id!==targetID);
+    localStorage.setItem("tasKy",JSON.stringify({cards:globalStore}));
+
+    if(tagname==="BUTTON"){
+        return taskcontainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    }
+    else{
+        return taskcontainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    }
+    //taskcontainer.removeChild(document.getElementById(targetID));
+
+
+
+};
+//issues
+
+//page refresh will cause the data to be deleted ->local storage ->5MB
+
+//API ->Application progamming interface 
+
+//localstorage ->Application
+//access programming via->Programming
+//interface as a middle man 
+
+//localstorage ->with some method ->javascript
+
+//features
+//delete the card
+//edit the card 
